da2df3d904b9243a8c8eb83b9148ef4bdac1c795
Luca Braglia <lbraglia@gmail.com>
2015-08-03 16:41:30 +0200

    move to a package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(name = 'pymimo',
+      packages = [
+          'pymimo',
+      ],
+      version = '0.1',
+      author = 'Luca Braglia',
+      author_email = 'lbraglia@gmail.com',
+      url = 'https://github.com/lbraglia/pymimo',
+      description = 'Python misc modules (mainly for personal use)',
+      classifiers = [
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+      ]
+)
